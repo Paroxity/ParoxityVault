491aa557ff3733f520916e5af9434bccf4943708
Ifera <jackmtaylor.jmt@gmail.com>
2021-03-20 15:12:57 +0500

    make vault db extendable

--- src/Paroxity/ParoxityVault/Database/VaultDatabase.php
+++ src/Paroxity/ParoxityVault/Database/VaultDatabase.php
@@ -7,21 +7,21 @@ use Paroxity\ParoxityVault\ParoxityVault;
 use Paroxity\ParoxityVault\Records\RecordsQuery;
 use poggit\libasynql\libasynql;
 
-final class VaultDatabase extends AwaitDatabase{
+class VaultDatabase extends AwaitDatabase{
 
-	public function __construct(ParoxityVault $plugin){
-		$config = $plugin->getConfig();
+	public function __construct(ParoxityVault $vault){
+		$config = $vault->getConfig();
 
 		$connector = libasynql::create(
-			$plugin,
+			$vault,
 			$config->get("database"),
 			[
 				"mysql" => "stmts/mysql/sql.sql"
 			]
 		);
 
-		$connector->loadQueryFile($plugin->getResource("stmts/mysql/records/records.sql"));
-		$connector->loadQueryFile($plugin->getResource("stmts/mysql/records/name_records.sql"));
+		$connector->loadQueryFile($vault->getResource("stmts/mysql/records/records.sql"));
+		$connector->loadQueryFile($vault->getResource("stmts/mysql/records/name_records.sql"));
 		$connector->executeGeneric(RecordsQuery::INIT_RECORDS);
 		$connector->executeGeneric(RecordsQuery::INIT_NAME_RECORDS);
 		$connector->waitAll();
