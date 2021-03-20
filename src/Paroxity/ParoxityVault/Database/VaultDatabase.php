ac36c897c06ef838c6ee1deaf310f34ed73ad42f
Ifera <jackmtaylor.jmt@gmail.com>
2021-03-20 16:28:31 +0500

    changes

--- src/Paroxity/ParoxityVault/Database/VaultDatabase.php
+++ src/Paroxity/ParoxityVault/Database/VaultDatabase.php
@@ -5,38 +5,62 @@ namespace Paroxity\ParoxityVault\Database;
 
 use Paroxity\ParoxityVault\ParoxityVault;
 use Paroxity\ParoxityVault\Records\RecordsQuery;
-use pocketmine\utils\SingletonTrait;
 use poggit\libasynql\libasynql;
 
 final class VaultDatabase extends AwaitDatabase{
 
-	use SingletonTrait;
+	public const TYPE_MYSQL = "mysql";
+	public const TYPE_SQLITE = "sqlite";
 
-	public function __construct(ParoxityVault $vault){
-		$config = $vault->getConfig();
+	private static string $type = self::TYPE_MYSQL;
+
+	public function __construct(ParoxityVault $plugin){
+		$config = $plugin->getConfig();
+
+		self::$type = (string) $config->getNested("database.type", "mysql");
 
 		$connector = libasynql::create(
-			$vault,
+			$plugin,
 			$config->get("database"),
 			[
 				"mysql" => "stmts/mysql/sql.sql"
 			]
 		);
 
-		$connector->loadQueryFile($vault->getResource("stmts/mysql/records/records.sql"));
-		$connector->loadQueryFile($vault->getResource("stmts/mysql/records/name_records.sql"));
+		parent::__construct($plugin, $connector); // this needs to be called before the next
+
+		self::loadQueryFile($plugin->getResource("stmts/mysql/records/records.sql"));
+		self::loadQueryFile($plugin->getResource("stmts/mysql/records/name_records.sql"));
+
 		$connector->executeGeneric(RecordsQuery::INIT_RECORDS);
 		$connector->executeGeneric(RecordsQuery::INIT_NAME_RECORDS);
 		$connector->waitAll();
+	}
 
-		parent::__construct($vault, $connector);
-		self::setInstance($this);
+	/**
+	 * Returns the database type selected in the config
+	 *
+	 * Either TYPE_MYSQL or TYPE_SQLITE
+	 */
+	public static function getType(): string{
+		return self::$type;
+	}
+
+	/**
+	 * Only load queries of the database type selected
+	 *
+	 * @param resource $fh
+	 */
+	public static function loadQueryFile($fh, string $type = self::TYPE_MYSQL, string $fileName = null) : void{
+		if(self::$type !== $type){
+			return;
+		}
+
+		ParoxityVault::getInstance()->getDatabase()->getConnector()->loadQueryFile($fh, $fileName);
 	}
 
 	public function close(): void{
 		$this->connector->close();
 		$this->connector->waitAll();
-
-		self::reset();
 	}
 }
